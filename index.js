4653c27b9958064f80af4199ebde898054142a69
jbmagination <26027089+jbmagination@users.noreply.github.com>
2020-01-31 18:45:29 -0500

    Oopsie poopsie #2
    I managed to forget to add commands that I added in during testing
    
    Which was supposed to be a part of release
    
    AND I need to update package.json with the new version number later
    
    Y A Y

--- index.js
+++ index.js
@@ -53,122 +53,154 @@ function onMessageHandler (target, context, msg, self) {
       client.say(target, `/me ${context.username} made the character jump!`); // This tells the player they successfully jumped.
       console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); // This logs in the console that we executed the command!
     } else // This separates the jump command from other commands, and will be used frequently.
-    // Now we'll make the attack command.
-    if (commandName.toLowerCase() === 'attack') { 
-        robot.keyTap("x"); 
-        client.say(target, `/me ${context.username} made the character attack!`); 
-        console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-      } else
-      if (commandName.toLowerCase() === 'jumpattack') { 
-        robot.keyTap("z"); 
-        robot.keyTap("x"); 
-        client.say(target, `/me ${context.username} made the character attack in mid-air!`); 
-        console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-      } else
-    if (commandName.toLowerCase() === 'right') { 
+ // Now we'll make the attack command.
+ if (commandName.toLowerCase() === 'attack') { 
+  robot.keyTap("x"); 
+  client.say(target, `/me ${context.username} made the character attack!`); 
+  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+} else
+if (commandName.toLowerCase() === 'jumpattack') { 
+  robot.keyTap("z"); 
+  robot.keyTap("x"); 
+  client.say(target, `/me ${context.username} made the character attack in mid-air!`); 
+  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+} else
+if (commandName.toLowerCase() === 'right') { 
+robot.keyTap("right"); 
+client.say(target, `/me ${context.username} made the character move right!`); 
+console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+} else
+if (commandName.toLowerCase() === 'hardright') { 
+  robot.keyTap("right"); 
+  robot.keyTap("right");
+  robot.keyTap("right");
+  robot.keyTap("right");
+  robot.keyTap("right");
+  client.say(target, `/me ${context.username} made the character REALLY move right!!!`); 
+  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+} else
+if (commandName.toLowerCase() === 'left') { 
+  robot.keyTap("left"); 
+  client.say(target, `/me ${context.username} made the character move left!`); 
+  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+} else
+if (commandName.toLowerCase() === 'hardleft') { 
+    robot.keyTap("left"); 
+    robot.keyTap("left");
+    robot.keyTap("left");
+    robot.keyTap("left");
+    robot.keyTap("left");
+    client.say(target, `/me ${context.username} made the character REALLY move left!!!`); 
+    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+  } else 
+  if (commandName.toLowerCase() === 'jumpleft') { 
+    robot.keyTap("z");
+    robot.keyTap("left"); 
+    client.say(target, `/me ${context.username} made the character move left and jump!`); 
+    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+  } else
+  if (commandName.toLowerCase() === 'jumphardleft') { 
+      robot.keyTap("z");
       robot.keyTap("right"); 
-      client.say(target, `/me ${context.username} made the character move right!`); 
+      robot.keyTap("left");
+      robot.keyTap("left");
+      robot.keyTap("left");
+      robot.keyTap("left");
+      client.say(target, `/me ${context.username} made the character REALLY move left AND JUMP!!!`); 
+      console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+    } else 
+    if (commandName.toLowerCase() === 'jumpright') { 
+      robot.keyTap("z");
+      robot.keyTap("right"); 
+      client.say(target, `/me ${context.username} made the character move right and jump!`); 
       console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
     } else
-    if (commandName.toLowerCase() === 'hardright') { 
+    if (commandName.toLowerCase() === 'jumphardright') { 
+        robot.keyTap("z");
         robot.keyTap("right"); 
         robot.keyTap("right");
         robot.keyTap("right");
         robot.keyTap("right");
         robot.keyTap("right");
-        client.say(target, `/me ${context.username} made the character REALLY move right!!!`); 
+        client.say(target, `/me ${context.username} made the character REALLY move right AND JUMP!!!`); 
         console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-      } else
-      if (commandName.toLowerCase() === 'left') { 
-        robot.keyTap("left"); 
-        client.say(target, `/me ${context.username} made the character move left!`); 
+      } else 
+  // Now let's focus on player 2!
+  if (commandName.toLowerCase() === 'p2') { 
+      robot.keyTap("f2"); 
+      client.say(target, `/me ${context.username} made toggled player 2!`); 
+      console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+    } else
+    if (commandName.toLowerCase() === 'p2up') { 
+        robot.keyTap("w");
+        client.say(target, `/me ${context.username} made player 2 move up!`); 
         console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-      } else
-      if (commandName.toLowerCase() === 'hardleft') { 
-          robot.keyTap("left"); 
-          robot.keyTap("left");
-          robot.keyTap("left");
-          robot.keyTap("left");
-          robot.keyTap("left");
-          client.say(target, `/me ${context.username} made the character REALLY move left!!!`); 
+      } else 
+      if (commandName.toLowerCase() === 'p2hardup') { 
+          robot.keyTap("w"); 
+          robot.keyTap("w");
+          robot.keyTap("w");
+          robot.keyTap("w");
+          robot.keyTap("w");
+          client.say(target, `/me ${context.username} made player 2 REALLY move up!!!`); 
+          console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+        } else
+        if (commandName.toLowerCase() === 'p2left') { 
+          robot.keyTap("d");
+          client.say(target, `/me ${context.username} made player 2 move left!`); 
           console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
         } else 
-        // Now let's focus on player 2!
-        if (commandName.toLowerCase() === 'p2') { 
-            robot.keyTap("f2"); 
-            client.say(target, `/me ${context.username} toggled player 2!`); 
+        if (commandName.toLowerCase() === 'p2hardleft') { 
+            robot.keyTap("d"); 
+            robot.keyTap("d");
+            robot.keyTap("d");
+            robot.keyTap("d");
+            robot.keyTap("d");
+            client.say(target, `/me ${context.username} made player 2 REALLY move left!!!`); 
             console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
           } else
-          if (commandName.toLowerCase() === 'p2up') { 
-              robot.keyTap("w");
-              client.say(target, `/me ${context.username} made player 2 move up!`); 
+          if (commandName.toLowerCase() === 'p2down') { 
+            robot.keyTap("s");
+            client.say(target, `/me ${context.username} made player 2 move down!`); 
+            console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+          } else 
+          if (commandName.toLowerCase() === 'p2harddown') { 
+              robot.keyTap("s"); 
+              robot.keyTap("s");
+              robot.keyTap("s");
+              robot.keyTap("s");
+              robot.keyTap("s");
+              client.say(target, `/me ${context.username} made player 2 REALLY move down!!!`); 
+              console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+            } else
+            if (commandName.toLowerCase() === 'p2right') { 
+              robot.keyTap("a");
+              client.say(target, `/me ${context.username} made player 2 move right!`); 
               console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
             } else 
-            if (commandName.toLowerCase() === 'p2hardup') { 
-                robot.keyTap("w"); 
-                robot.keyTap("w");
-                robot.keyTap("w");
-                robot.keyTap("w");
-                robot.keyTap("w");
-                client.say(target, `/me ${context.username} made player 2 REALLY move up!!!`); 
-                console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-              } else
-              if (commandName.toLowerCase() === 'p2right') { 
-                robot.keyTap("d");
-                client.say(target, `/me ${context.username} made player 2 move left!`); 
-                console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-              } else 
-              if (commandName.toLowerCase() === 'p2hardright') { 
-                  robot.keyTap("d"); 
-                  robot.keyTap("d");
-                  robot.keyTap("d");
-                  robot.keyTap("d");
-                  robot.keyTap("d");
-                  client.say(target, `/me ${context.username} made player 2 REALLY move left!!!`); 
-                  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-                } else
-                if (commandName.toLowerCase() === 'p2down') { 
-                  robot.keyTap("s");
-                  client.say(target, `/me ${context.username} made player 2 move down!`); 
-                  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-                } else 
-                if (commandName.toLowerCase() === 'p2harddown') { 
-                    robot.keyTap("s"); 
-                    robot.keyTap("s");
-                    robot.keyTap("s");
-                    robot.keyTap("s");
-                    robot.keyTap("s");
-                    client.say(target, `/me ${context.username} made player 2 REALLY move down!!!`); 
-                    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-                  } else
-                  if (commandName.toLowerCase() === 'p2left') { 
-                    robot.keyTap("a");
-                    client.say(target, `/me ${context.username} made player 2 move right!`); 
-                    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-                  } else 
-                  if (commandName.toLowerCase() === 'p2hardleft') { 
-                    robot.keyTap("a"); 
-                    robot.keyTap("a");
-                    robot.keyTap("a");
-                    robot.keyTap("a");
-                    robot.keyTap("a");
-                    client.say(target, `/me ${context.username} made player 2 REALLY move right!!!`); 
-                    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
-                  } else
-                  if (commandName.toLowerCase() === 'everyone say hello! or: hi | hey | sup | yo | hola | hai | greetings | salutations | hallo | howdy | annyeong | aloha | konichiwa') {
-                    // The above line is the message that the Choice Chamber chat bot sends in the chat when on the main menu screen.
-                    if (context.username === chatbot) { // This checks to see if the Choice Chamber chat bot name, or anyone who is attempting to play as or impersonate one, matches the existing chat bot.
-                      robot.keyTap("z"); // By default it selects "play", so pressing Z will start the game again.
-                      console.log(`* New game`)
-                    } else { console.log(`* User ${context.username} attempted to send new game message`)}
-                } else
-                  // now, for a command that does NOTHING in-game!!!
-                  if (commandName.toLowerCase() === '!cchelp') {
-                    client.say(target, `${context.username} Please visit https://jbmagination.com/TwitchPlaysCC`)
-                  } else { 
-      console.log(`* Unknown command sent by ` + context.username + `: ` + commandName.toUpperCase()); 
-    }
-  }
+            if (commandName.toLowerCase() === 'p2hardright') { 
+              robot.keyTap("a"); 
+              robot.keyTap("a");
+              robot.keyTap("a");
+              robot.keyTap("a");
+              robot.keyTap("a");
+              client.say(target, `/me ${context.username} made player 2 REALLY move right!!!`); 
+              console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); 
+            } else
+            if (commandName.toLowerCase() === 'everyone say hello! or: hi | hey | sup | yo | hola | hai | greetings | salutations | hallo | howdy | annyeong | aloha | konichiwa') {
+              // The above line is the message that the Choice Chamber chat bot sends in the chat when on the main menu screen.
+              if (context.username === chatbot) { // This checks to see if the Choice Chamber chat bot name, or anyone who is attempting to play as or impersonate one, matches the existing chat bot.
+                robot.keyTap("z"); // By default it selects "play", so pressing Z will start the game again.
+                console.log(`* New game`)
+              } else { console.log(`* User ${context.username} attempted to send new game message`)}
+          } else
+            // now, for a command that does NOTHING in-game!!!
+            if (commandName.toLowerCase() === '!cchelp') {
+              client.say(target, `${context.username} Please visit https://jbmagination.com/TwitchPlaysCC`)
+            } else { 
+console.log(`* Unknown command sent by ` + context.username + `: ` + commandName.toUpperCase()); 
+}
+}
 
 // Finally, we can connect to Twitch and let this bad boy run.
 client.connect();
