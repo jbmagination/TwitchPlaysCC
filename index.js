9cc3833c57b2fea4d7e876fc8b74548fa9014f30
Sojobo <aaron.otaku@gmail.com>
2020-02-16 16:29:40 +0000

    Tidy up, add bridge mode & toggle players

--- index.js
+++ index.js
@@ -1,150 +1,152 @@
 // First of all, we get tmi.js, the Twitch API...
-var twitch = require('tmi.js');
+const twitch = require("tmi.js");
+
 // Then, we get robotjs. This puts in our keyboard input to control the player.
-var robot = require('robotjs');
+const robot = require("robotjs");
+
 // In order to read the config file, we need to get JSON5.
-var json5 = require('json5');
-require('json5/lib/register');
+require("json5/lib/register");
+
 // Now, let's actually call for the file.
-var config = require('./config.json5');
+const config = require("./config.json5");
 // We have the file now, let's simplify our lives now.
-var botname = config.bot;
-var oauth = config.oauth;
-var streamer = config.username;
-var powers = config.powers;
-var backed = config.backed;
-var chatbot = config.chatbot;
+const botname = config.bot;
+const oauth = config.oauth;
+const streamer = config.username;
+// var powers = config.powers;
+// var backed = config.backed;
+const chatbot = config.chatbot;
+
+const controlp1 = config.controlp1;
+const controlp2 = config.controlp2;
+let p2bridged = false;
+
+let commands = {};
+if (controlp1 && controlp2) { // Default keys for player 1 & player 2 mode
+    commands = {
+        left: ["left", "character move left"],
+        right: ["right", "character move right"],
+        jump: ["z", "character jump"],
+        attack: ["x", "character attack"],
+
+        p2: ["f2", "toggled the gizmo"],
+        p2left: ["a", "gizmo move left"],
+        p2right: ["d", "gizmo move right"],
+        p2up: ["w", "gizmo move up"],
+        p2down: ["s", "gizmo move down"],
+        p2jump: ["r", "gizmo toggle bridge-mode"],
+        p2attack: ["t", "gizmo attack"]
+    };
+} else if (!controlp1 && controlp2) { // Player 1 disabled, Player 2 only mode
+    commands = {
+        left: ["a", "gizmo move left"],
+        right: ["d", "gizmo move right"],
+        up: ["w", "gizmo move up"],
+        down: ["s", "gizmo move down"],
+        jump: ["r", "gizmo toggle bridge-mode"],
+        attack: ["t", "gizmo attack"]
+    };
+} else if (controlp1 && !controlp2) { // Player 2 disabled, Player 1 only mode
+    commands = {
+        left: ["left", "character move left"],
+        right: ["right", "character move right"],
+        jump: ["z", "character jump"],
+        attack: ["x", "character attack"]
+    };
+}
+
 // We need the actual configurations to be used by the Twitch API. Let's go ahead and sort that out.
 const twitchconfig = {
     identity: {
-      username: botname,
-      password: oauth
+        username: botname,
+        password: oauth
     },
     channels: [
-      streamer
+        streamer
     ]
-  };
+};
+
 // We NEED to set up our client that will be used by Twitch.
+// eslint-disable-next-line new-cap
 const client = new twitch.client(twitchconfig);
 // And let the code begin!
 
 // So, firstly we need to make sure that we can make certain things occur on certain events.
 // In this case, we detect when the bot connects to Twitch, and when someone sends a message in chat.
-client.on('connected', onConnectedHandler);
-client.on('message', onMessageHandler);
+client.on("connected", onConnectedHandler);
+client.on("message", onMessageHandler);
 
 // Let's set up being connected first!
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
-    client.say(streamer, `/me [TWITCH PLAYS] Bot is online!`);
-  }
+    client.say(streamer, "/me [TWITCH PLAYS] Bot is online!");
+}
 
 // Let's set up the chat bot. Here we go!!!
-function onMessageHandler (target, context, msg, self) {
-    if (self) { return; } // We don't want it to trigger itself, so let's make sure that it doesn't do that.
-  
+function onMessageHandler(target, context, msg, self) {
+    let commandsExecuted = 0;
+    let lastcmd = "";
+
+    if (self) {
+        return;
+    } // We don't want it to trigger itself, so let's make sure that it doesn't do that.
+
     // Now, we'll need to get rid of any whitespace in our messages.
     const commandName = msg.trim();
 
+    if (commandName === "everyone say hello! or: hi | hey | sup | yo | hola | hai | greetings | salutations | hallo | howdy | annyeong | aloha | konichiwa") {
+        if (context.username === chatbot) { // This checks to see if the Choice Chamber chat bot name, or anyone who is attempting to play as or impersonate one, matches the existing chat bot.
+            robot.keyTap("z"); // By default it selects "play", so pressing Z will start the game again.
+            console.log("* New game");
+        } else {
+            console.log(`* User ${context.username} attempted to send new game message`);
+        }
+        return;
+    } else if (commandName === "!cchelp") {
+        client.say(target, `[TWITCH PLAYS] ${context.username} Please visit https://jbmagination.com/TwitchPlaysCC`);
+        return;
+    }
+
     // We want people to be able to run multiple commands and have it run them, so let's get that going...
-    var commandList = commandName.split(/\s+/g);
+    const commandList = commandName.split(/\s+/g);
 
     // Now let's set up our commands.
+    let keepGoing = true;
     commandList.forEach(commandName => {
+        if (!keepGoing) {
+            return;
+        }
 
-  // Let's do our jump command first.
-  if (commandName.toLowerCase() === 'jump') { // This starts a new event, telling the bot that someone ran the command 'jump'!
-  robot.keyTap("z"); // This is where robot.js comes in. We're going to press Z, the jump button, to jump.
-  client.say(target, `/me [TWITCH PLAYS] ${context.username} made the character jump!`); // This tells the player they successfully jumped.
-  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`); // This logs in the console that we executed the command!
-} else // This separates the jump command from other commands, and will be used frequently.
+        let actioned = true;
+        const cmd = commandName.toLowerCase();
 
+        if (typeof commands[cmd] !== "undefined") {
+            if (commands[cmd][0] === "r") {
+                robot.keyToggle("r", p2bridged ? "up" : "down");
+                p2bridged = !p2bridged;
+            } else {
+                robot.keyTap(commands[cmd][0]);
+            }
+            lastcmd = commands[cmd];
+            commandsExecuted++;
+        } else {
+            console.log(`* Unknown command sent by ${context.username}: ${commandName.toUpperCase()}`);
+            actioned = false;
+            keepGoing = false;
+            return;
+        }
 
+        if (actioned) {
+            console.log(`* ${context.username} executed the ${commandName.toUpperCase()} command`);
+        }
+    });
 
-// Now we'll make the attack command.
-if (commandName.toLowerCase() === 'attack') { 
-    robot.keyTap("x");  
-    client.say(target, `/me [TWITCH PLAYS] ${context.username} made the character attack!`); 
-    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-  } else
-
-
-
-if (commandName.toLowerCase() === 'right') { 
-  robot.keyTap("right"); 
-  client.say(target, `/me  [TWITCH PLAYS] ${context.username} made the character move right!`); 
-  console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-} else
-
-
-
-  if (commandName.toLowerCase() === 'left') { 
-    robot.keyTap("left"); 
-    client.say(target, `/me [TWITCH PLAYS] ${context.username} made the character move left!`); 
-    console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-  } else
-
-
-    // Now let's focus on player 2!
-    if (commandName.toLowerCase() === 'p2') { 
-        robot.keyTap("f2"); 
-        client.say(target, `/me [TWITCH PLAYS] ${context.username} made toggled player 2!`); 
-        console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-      } else
-
-
-
-      if (commandName.toLowerCase() === 'p2up') { 
-          robot.keyTap("w");
-          client.say(target, `/me [TWITCH PLAYS] ${context.username} made player 2 move up!`); 
-          console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-        } else 
-
-
-
-          if (commandName.toLowerCase() === 'p2left') { 
-            robot.keyTap("d");
-            client.say(target, `/me [TWITCH PLAYS] ${context.username} made player 2 move left!`); 
-            console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-          } else 
-
-
-
-            if (commandName.toLowerCase() === 'p2down') { 
-              robot.keyTap("s");
-              client.say(target, `/me [TWITCH PLAYS] ${context.username} made player 2 move down!`); 
-              console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-            } else 
-           
-
-
-              if (commandName.toLowerCase() === 'p2right') { 
-                robot.keyTap("a");
-                client.say(target, `/me [TWITCH PLAYS] ${context.username} made player 2 move right!`); 
-                console.log(`* ${context.username} executed the ` + commandName.toUpperCase() + ` command`);
-              } else 
-            
-
-
-              if (commandName.toLowerCase() === 'everyone say hello! or: hi | hey | sup | yo | hola | hai | greetings | salutations | hallo | howdy | annyeong | aloha | konichiwa') {
-                // The above line is the message that the Choice Chamber chat bot sends in the chat when on the main menu screen.
-                if (context.username === chatbot) { // This checks to see if the Choice Chamber chat bot name, or anyone who is attempting to play as or impersonate one, matches the existing chat bot.
-                  robot.keyTap("z"); // By default it selects "play", so pressing Z will start the game again.
-                  console.log(`* New game`)
-                } else { console.log(`* User ${context.username} attempted to send new game message`)}
-            } else
-
-
-
-              // now, for a command that does NOTHING in-game!!!
-              if (commandName.toLowerCase() === '!cchelp') {
-                client.say(target, `[TWITCH PLAYS] ${context.username} Please visit https://jbmagination.com/TwitchPlaysCC`)
-              } else { 
-  console.log(`* Unknown command sent by ` + context.username + `: ` + commandName.toUpperCase()); 
+    if (commandsExecuted === 1) {
+        client.say(target, `/me  [TWITCH PLAYS] ${context.username} made the ${lastcmd[1]}!`);
+    } else if (commandsExecuted > 1) {
+        client.say(target, `/me [TWITCH PLAYS] ${context.username} executed ${commandsExecuted} commands`);
+    }
 }
-      })};    
 
 // Finally, we can connect to Twitch and let this bad boy run.
-client.connect()
-
-// And there you have it!
+client.connect();
